8841dabe44964275c4fe31ea538fa3e009dffdc7
MikhailKarpAndr <129546659+MikhailKarpAndr@users.noreply.github.com>
2023-03-31 23:54:52 +0300

    Delete lesson1 directory

--- lesson1/index.js
+++
@@ -1,5 +0,0 @@
-document.querySelector('.add').onclick = function () {
-    let num1 = parseInt(document.querySelector('#num1').value);
-    let num2 = parseInt(document.querySelector('#num2').value);
-    alert(num1 + num2);
-}
